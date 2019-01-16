22075035dedbcad501dd9b9dc6d553213387c65c
Michael George <mgeorge9492@gmail.com>
2019-01-15 22:47:35 -0800

    need to deploy to heroku and do jsdoc

---
+++ src/models/bookshelves.js
@@ -0,0 +1,7 @@
+'use strict';
+const bookshelfSchema = require('./bookshelf-schema.js');
+const DataModel = require('./model.js');
+
+class Bookshelf extends DataModel { }
+
+module.exports = new Bookshelf(bookshelfSchema);
